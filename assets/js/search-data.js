dff51cf123ef610bb4af71821aa1b021cec4e1f5
camilo-rojo <camilo.rodriguez.ronderos@gmail.com>
2024-12-30 08:47:53 +0000

    Deploying to gh-pages from @ camilo-rojo/camilo-rojo.github.io@a73dbd1d572c66814700910e97f5d35542ad89a3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-",
+    title: "",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Click on the icon to download a copy of my CV.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -36,13 +36,6 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/people/";
-          },
-        },{id: "nav-submenus",
-          title: "submenus",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/_pages/dropdown/";
           },
         },{id: "post-a-post-with-image-galleries",
       
